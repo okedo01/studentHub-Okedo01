121398036db19b285217c8ae50b6818006993f3e
Okedo01 <eliahmwelangi01@gmail.com>
2025-06-30 16:29:12 +0300

    form

--- src/Register.tsx
+++ src/Register.tsx
@@ -1,48 +1,72 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
-import {
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage
-} from './components/ui/form';
-import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom'
+
+const { id } = useParams();
+const formID = Number(id);
 
 type formData = {
-    username: string;
+    email: string
+    password: string
+    confirmPassword: string
 }
 
 const Register: React.FC = () => {
-    const { id } = useParams();
-    const courseID = Number(id);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+    getValues,
+  } = useForm<formData>();
 
-    const form = useForm<formData>();
-    return (
-        <div className="bg-amber-300">
-            <h2 className="text-xl font-bold mb-4">
-                Register for Course ID: {courseID}
-            </h2>
-            {/* <FormField
-                control={form.control}
-                name="username"
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>{id}</FormLabel>
-                        <FormControl>
-                            <input placeholder="shadcn" {...field} />
-                        </FormControl>
-                        <FormDescription>This is your public display name.{id}</FormDescription>
-                        <FormMessage />
-                    </FormItem>
-                )}
-            /> */}
+  const onSubmit = async (data: formData) => {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    reset();
+  }
 
-            <input type="text" placeholder="First Name" />
-            <input type="text" placeholder="Last Name" />
-        </div>
-    )
+  return (
+    <div>
+      <h1>Register for course ID: { formID }</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input {
+            ...register("email", {
+              required: "Email is required",
+            })
+          } 
+          type="email" placeholder='Enter email' />
+          { errors.email && (
+            <p className='error'>{ `${errors.email.message}` }</p>
+          )}
+          <input {
+            ...register("password", {
+              required: "Password is required",
+              minLength: {
+                value: 7,
+                message: "Password must be 7 characters"
+              }
+            })
+          }
+          type="password" placeholder='Enter password' />
+          { errors.password && (
+            <p className='error'>{`${errors.password.message}`}</p>
+          )}
+          <input {
+            ...register("confirmPassword", {
+              required: "Confirm password is required",
+              validate: (value) => 
+                value === getValues("password") || "Passwords do not match"
+              
+            })
+          }
+          type="password" placeholder='Confirm password' />
+          { errors.confirmPassword && (
+            <p className='error'>{`${errors.confirmPassword.message}`}</p>
+          )}
+          <button type='submit' disabled={isSubmitting}>Submit</button>
+        </form>
+    </div>
+  )
 }
 
 export default Register
