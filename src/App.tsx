aae9d47bf023dd9ae7fade24fb032c40af866899
Okedo01 <eliahmwelangi01@gmail.com>
2025-06-30 14:51:54 +0300

    Add HubLayout Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import Home from './Pages/Home'
 import './App.css'
-import Navbar from './Navbar'
 import { Routes, Route } from 'react-router-dom'
 import About from './Pages/About'
 import { Contact } from 'lucide-react'
 import Cards from './Cards'
 import Register from './Register'
+import NotFound from './NotFound'
+import HubLayout from './HubLayout'
 
 function App() {
 
   return (
     <div>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/courses" element={<Cards />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<HubLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/courses" element={<Cards />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
       </Routes>
     </div>
   )
