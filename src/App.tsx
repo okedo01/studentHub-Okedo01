e4c071a9d3d7774f1e8bbc0e058004a1e927db96
Okedo01 <eliahmwelangi01@gmail.com>
2025-07-02 17:16:44 +0300

    Update App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,51 @@
-import Home from './Pages/Home'
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import About from './Pages/About'
-import Cards from './Cards'
-import Register from './Register'
-import NotFound from './NotFound'
-import HubLayout from './HubLayout'
-import Contact from './Pages/Contact'
-import StudentList from './StudentList'
+// import Home from './Pages/Home'
+// import './App.css'
+// import { Routes, Route } from 'react-router-dom'
+// import About from './Pages/About'
+// import Cards from './Cards'
+// import Register from './Register'
+// import NotFound from './NotFound'
+// import HubLayout from './HubLayout'
+// import Contact from './Pages/Contact'
+// import StudentList from './StudentList'
+
+// function App() {
+
+//   return (
+//     <div>
+//       <Routes>
+//         <Route path="/" element={<HubLayout />}>
+//           <Route index element={<Home />} />
+//           <Route path="/about" element={<About />} />
+//           <Route path="/contact" element={<Contact />} />
+//           <Route path="/courses" element={<Cards />} />
+//           <Route path="/courses/:id" element={<Register />} />
+//           <Route path="/students/:id" element={<StudentList />} />
+//           <Route path="*" element={<NotFound />} />
+//         </Route>
+//       </Routes>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import Cards from './Cards';
+import Register from './Register';
+import NotFound from './NotFound';
+import HubLayout from './HubLayout';
+import StudentList from './StudentList';
+import Dashboard from './Dashboard'; 
+import ProtectedRoute from './ProtectedRoute'; 
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-
   return (
     <div>
       <Routes>
@@ -21,11 +56,21 @@ function App() {
           <Route path="/courses" element={<Cards />} />
           <Route path="/courses/:id" element={<Register />} />
           <Route path="/students/:id" element={<StudentList />} />
+
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
