2b1df7403a559285a212e07f3a5bc0a5cb9133cd
Okedo01 <eliahmwelangi01@gmail.com>
2025-07-08 11:06:31 +0300

    Button

--- src/LogoutBtn.tsx
+++ src/LogoutBtn.tsx
@@ -13,8 +13,9 @@ const LogoutButton: React.FC = () => {
 
   return (
     <button
+      style={{color: "white"}}
+      className="fixed top-4 right-4 bg-gray-700 px-4 py-2 rounded cursor-pointer"
       onClick={handleLogout}
-      className="fixed top-4 right-4 bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700"
     >
       Logout
     </button>
